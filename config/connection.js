f0f0e89041fc76738c6f15d084ce33324d62db1e
Marucs Hilaire <marcushilaire@The-Mac.fios-router.home>
2018-03-19 22:50:34 -0400

    heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
+var connection;
+if(process.env.JAWSDB_URL){
+    connection=mysql.createConnection(process.env.JAWSDB_URL)}
+    else{
+connection = mysql.createConnection({
     port: 3306,
     host: "localhost",
     user: "root",
     password: "",
     database: "burgersDB"
 });
+}
 connection.connect(function(err){
     if (err) {
         console.error("error connecting: " + err.stack);
